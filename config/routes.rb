58bd21914483e222bf3c47f55cc4f80a9c1236ec
Dean DeHart <DeHartDean@gmail.com>
2023-08-27 20:14:09 -0400

    Change routes to use /api/v1
    This fix probably breaks stuff tho.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  resources :posts
+  # API routes should be in /api/v1
+  namespace :api do
+    namespace :v1 do
+      resources :posts
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
